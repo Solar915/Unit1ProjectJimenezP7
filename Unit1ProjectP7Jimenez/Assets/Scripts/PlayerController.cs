6b223d18776d9daa89fc6c0114f5143d189a16ef
Ethan Jimenez <723068@student.dusd.net>
2024-03-06 12:19:02 -0800

    I have changed the speed of my car and have given it full player control
    For my game, I have given player control to my car and have changed it's speed

--- Unit1ProjectP7Jimenez/Assets/Scripts/PlayerController.cs
+++ Unit1ProjectP7Jimenez/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
    public float speed = 5.0f;
+   public float turnSpeed;
+   public float horizontalInput;
+    public float forwardInput;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +18,11 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
         // We'll move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
